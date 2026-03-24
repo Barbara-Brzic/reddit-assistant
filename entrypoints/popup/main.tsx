6d37c9a1b93b9e660f01827fa1be46acdd28c81c
Barbara Brzic <barbara.brzic97@gmail.com>
2026-03-24 11:03:21 +0100

    setup prettier

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -4,6 +4,6 @@ import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className={"text-2xl text-red-500"}>Hello world</h1>
-  </React.StrictMode>,
+    <h1 className={'text-2xl text-red-500'}>Hello world</h1>
+  </React.StrictMode>
 );
