bac53d7ff77ea6a0907ee496bcf79697d8a3edfb
Barbara Brzic <barbara.brzic97@gmail.com>
2026-03-24 10:56:51 +0100

    setup tailwindcss, remove unused files

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: ({ manifestVersion }) => {
+    return {
+      manifest_version: manifestVersion,
+      name: "WXT Example",
+      description: "WXT Example",
+      version: "1.0.0",
+      permissions: ["storage", "tabs"],
+    };
+  },
 });
